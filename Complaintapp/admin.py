8dc9a371abfb5ad4f474ebe9a26cd42f2e99286f
Deepak Shkaya <deepakshakya4455@gmail.com>
2020-04-27 12:37:51 +0530

    Fistcommit

---
+++ Complaintapp/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from Complaintapp.models import ComplaintInfo,WardenInfo
+
+# Register your models here.
+class WardenAdmin(admin.ModelAdmin):
+    fields=['name','year','enrollment_num','room_num','complaintbox']
+    search_fields=['room_num','enrollment_num']
+    list_display=['name','year','enrollment_num','room_num','complaintbox']
+admin.site.register(ComplaintInfo,WardenAdmin)
+admin.site.register(WardenInfo)
---
+++ Complaintapp/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class ComplaintInfo(models.Model):
+    name=models.CharField(max_length=255)
+    year=models.CharField(max_length=90)
+    enrollment_num=models.IntegerField(unique=True)
+    room_num=models.IntegerField(unique=True)
+    complaintbox=models.TextField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class WardenInfo(models.Model):
+    image=models.ImageField(upload_to="media", blank=True)
+    warden_name=models.CharField(max_length=255,null=True)
+    designation=models.CharField(max_length=255,null=True)
+
+    def __str__(self):
+        return self.warden_name
